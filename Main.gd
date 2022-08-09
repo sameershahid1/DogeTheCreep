35b22ed607abc764291ab198c154bde1c4678c9f
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-09 23:50:42 +0500

    Player and Enemy size issue Resolved and Main Menu is Added

--- Main.gd
+++ Main.gd
@@ -1,14 +1,28 @@
 extends Node2D
 
 export (PackedScene) var mob_scene
+var score=0
 
+#When this function is called the ScoreTime is started
+func new_game():
+	score=0
+	$HUD.update_score(score)
+	$StartTimer.start()
+	$HUD.show_message("Get ready.......")
+	yield($StartTimer,"timeout")#Unitl the StartTimer passed 2 seconds the game will not be started
+	$ScoreTimer.start()
+	$MobTimer.start()
+
+#This function is called when ScoreTimer is Stoped	
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func ready():
 	randomize()
-	var kk=mob_scene.collision_layer
-	print(kk)
 
-
+#This functin is called every 0.5 seconds
 func _on_MobTimer_timeout():
 	var mob_spawn_location=$MobPath/MobSpanLocation
 	mob_spawn_location.unit_offset=randf()#Generating Location between 0 to 1
@@ -19,10 +33,15 @@ func _on_MobTimer_timeout():
 	mob.position=mob_spawn_location.position#Getting the locatin of the PathFollowNode and setting that location in the mob
 	var Direction=mob_spawn_location.rotation+(PI/2)#Rotating the Mobs 90 Degree
 	Direction+=rand_range(-PI/4,PI/4)#By adding this the mob will move between 
-	mob.rotation=Direction	
+	mob.rotation=Direction
 	
 	var Velocity=Vector2(rand_range(mob.min_speed,mob.max_speed),0)
 	mob.linear_velocity=Velocity.rotated(Direction)
+
+#Incrementing the score after 1 second
+func _on_ScoreTimer_timeout():
+	score+=1
+	$HUD.update_score(score)
 
 
 
